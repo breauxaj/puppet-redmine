35acd20af23aed81028a8a32ae0998e9f7750fb6
Adam Breaux <breauxaj@gmail.com>
2013-12-24 17:34:41 -0500

    a good start

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,25 @@
 class redmine (
   $target = '/var/vhost',
 ) {
-  exec { 'pull-drush':
+  file { $target:
+    ensure => directory,
+  }
+
+  exec { 'pull-redmine':
     path    => '/bin:/usr/bin',
     command => "git clone https://github.com/redmine/redmine.git",
     cwd     => $target,
     creates => "${target}/redmine/.git',
     timeout => 10000,
-    require => Package['git','ruby'],
+    require => [ Package['git','ruby'], File[$target] ],
+  }
+
+  exec { 'redmine-dependencies':
+    path    => '/bin:/usr/bin',
+    command => 'bundle install',
+    cwd     => "${target}/redmine",
+    timeout => 10000,
+    require => Exec['pull-redmine'],
   }
 
 }
