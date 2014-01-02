1f4ef1e9fa9c08673eb9a3a1548345dccf0f3e97
Adam Breaux <breauxaj@gmail.com>
2014-01-02 10:58:21 -0500

    only cover mysql

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,17 @@
 define redmine::config (
-
+  $target = '/var/vhost',
+  $adapter = 'mysql',
+  $database = 'redmine',
+  $host = '127.0.0.1',
+  $username = 'redmine',
+  $password = 'r3dm1n#'
 ) {
+  file { "$target/redmine/config/database.yml":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('redmine/database.erb'),
+  }
 
 }
